dcec2cb3cf0a4c13151bd9dfb2bf50c6e64ca492
LG <lg@s-164-67-229-99.resnet.ucla.edu>
2015-07-02 02:46:39 -0700

    finished refactor/bugfix, tested, working for kmean

--- benchmark.py
+++ benchmark.py
@@ -11,6 +11,8 @@ import kmeans
 import parseHapmap as parseHap
 import copy
 import timeit
+import random
+import numpy as np
 
 
 def majorityPop(indivs, k):
@@ -41,74 +43,135 @@ def majorityPop(indivs, k):
 
 
 
-
-
-indivs, genoArr = parseHap.runParse()
-print("Using DEU, CHB and MKK") # TODO update if i change pops
-# though uneven pop size may be good to show weakness of kmeans
-
-
-# TODO refactor so for N in range(200) you could run benchmark and append its return values to appropriate lists
-# actual N = 94 + 171 + 165 for CHB + MKK + CEU
-# actual M = 20,000+
-# true K = 3 (CHB, MKK, CEU)
-
 def runBenchmark(N=200, M=10000, K=3):
-	# TODO change N. 
-	# varying N
-	kmeansTimes = []
-	kmeansMajPops = [] 		# kmeansMaxPops[s] is majority pop of cluster s
-	kmeansMajFracs = [] 	# kmeansMaxFracs[s] = % cluster s that is max 
+
+
 	# note that order of clusters will vary from run to run, so track which true pop with each cluster
 	#	along with true frac count
 
+	# sample N indivs without replacement, and also get first M snps for each geno
+	indices = random.sample(range(len(genoArr)), N)
+	indivs_copy = np.array([indivs[i] for i in indices])
+	for i in range(N):
+		indivs_copy[i].geno = np.array(indivs_copy[i].geno[:M])
+		indivs_copy[i].j = i 		# also update position in new indiv list
 
-	# sample N indivs without replacement
-	indices = random.choice(range(len(genoArr), N))
-	indivs_copy = indivs[indices]
-	genoArr_copy = genoArr[indices]
+	genoArr_copy = np.array([genoArr[i][:M] for i in indices])
 
 	# old version: just take first N since already shuffled
 	#indivs_copy = copy.deepcopy(indivs[:N])
 	#genoArr_copy = copy.deepcopy(genoArr[:N])
 
-	k = K
 
 	def kmeans_i():
 		'''zero input fxn for timeit'''
-		return kmeans.kmeans(indivs_copy, genoArr_copy, k)
+		return kmeans.kmeans(indivs_copy, genoArr_copy, K)
 
-	# timing kmeans
-	time = timeit.timeit(kmeans_i, number = 1) # only run once per data pt
-	kmeansTimes.append(time)
+	# timing kmeans, # run 10x per data pt, avg
+	kmeansTime = timeit.timeit(kmeans_i, number = 10)/10.0 
 
 	# measure with kmeans objective fxn
-	centers = kmeans.kmeans(indivs_copy, genoArr_copy, k, maxIter = 1000, verbose = True)
+	centers = kmeans.kmeans(indivs_copy, genoArr_copy, K, maxIter = 1000, verbose = True)
 	kmeansObj = kmeans.kmeansObj(indivs_copy, centers)
-	majPops, majFracs, clusterSizes = majorityPop(indivs_copy, k)
+	majPops, majFracs, clusterSizes = majorityPop(indivs_copy, K)
 
-	kmeansMajPops.append(majPops)
-	kmeansMajFracs.append(majFracs)
+	# majPops[s] = majority pop of cluster s
+	# majFracs[s] = % cluster s that is max 
 
-
-	# TODO should weight mean by size of cluster, not useful atm
-	# TODO don't weight homogeneity equal for cluster with 1 indiv and cluster with 1000
-	kmeansMajFrac_avg = 1.0*sum(kmeansMajFracs*kmeans)/k
-
-
-	return kmeansObj, kmeansMajFrac_avg, #kmeansTimes, kmeansMajPops, kmeansMajFracs
+	# TODO should weight mean by size of cluster?
+	# TODO is it ok to weight homogeneity equal for cluster with 1 indiv and cluster with 1000
+	kmeansMajFrac_avg = 1.0*sum(majFracs)/K
 
 
-# TODO print, or work with the variables interactively to make graphs
-# K-means,varying total number of individuals from 2 populations
-for N in range(20, 200, 10): # could change skip size to get more data
-	print("Now testing N = %d" % N)
-	runBenchmark(N, M, K)
+	return kmeansObj, kmeansMajFrac_avg, kmeansTime, majPops, majFracs
+
+
+
 		
 
+# actual N = 94 + 171 + 165 for CHB + MKK + CEU
+# actual M = 20109
+# true K = 3 (CHB, MKK, CEU)
+
+indivs, genoArr = parseHap.runParse()
+print("Using DEU, CHB and MKK") # TODO update if i change pops
+# though uneven pop size may be good to show weakness of kmeans
+'''
+M = 10000
+K = 3
+print("Benchmarking K-means, varying N with M = %d, K = %d" % (M, K))
+
+# lists of results
+kmeans_N = [] # the value of N used for the test
+kmeansObj_N = []
+kmeansMajFrac_avg_N = []
+kmeansTime_N = []
+kmeansMajPops_N = []
+kmeansMajFracs_N = []
+
+for N in range(20, 200, 10): # could change skip size to get more data
+	print("Now testing N = %d" % N)
+	kmeansObj, kmeansMajFrac_avg, kmeansTime, kmeansMajPops, kmeansMajFracs = runBenchmark(N, M, K)
+	kmeans_N.append(N)
+	kmeansObj_N.append(kmeansObj)
+	kmeansMajFrac_avg_N.append(kmeansMajFrac_avg)
+	kmeansTime_N.append(kmeansTime)
+	kmeansMajPops_N.append(kmeansMajPops)
+	kmeansMajFracs_N.append(kmeansMajFracs)
+
+# output file:
+print("writing kmeans_N results to file")
+with open("results_kmeans_N.txt", 'w') as outf:
+	outf.write('\t'.join(['N', 'kmeans_obj', 'kmeans_majFrac_avg', 'kmeans_time']) + '\n')
+	for i in range(len(kmeans_N)):
+		outf.write("\t".join(
+			[str(var) for var in [ kmeans_N[i], kmeansObj_N[i], kmeansMajFrac_avg_N[i], kmeansTime_N[i] ]]
+		) + "\n")
+
+with open("results_kmeans_N_majorities.txt", 'w') as outf:
+	for i in range(len(kmeans_N)):
+		outf.write("\n".join(str(var) for var in kmeansMajPops_N[i] + kmeansMajFracs_N[i]) + "\n")
+	outf.write("\n")
+'''
 
 
-# TODO also vary M
+# repeat for varying M
+N = 150
+K = 3
+print("Benchmarking K-means, varying M with N = %d, K = %d" % (N, K))
+
+# lists of results
+kmeans_M = [] # the value of N used for the test
+kmeansObj_M = []
+kmeansMajFrac_avg_M = []
+kmeansTime_M = []
+kmeansMajPops_M = []
+kmeansMajFracs_M = []
+
+for M in range(200, 20000, 200): 
+	print("Now testing M = %d" % M)
+	kmeansObj, kmeansMajFrac_avg, kmeansTime, kmeansMajPops, kmeansMajFracs = runBenchmark(N, M, K)
+	kmeans_M.append(M)
+	kmeansObj_M.append(kmeansObj)
+	kmeansMajFrac_avg_M.append(kmeansMajFrac_avg)
+	kmeansTime_M.append(kmeansTime)
+	kmeansMajPops_M.append(kmeansMajPops)
+	kmeansMajFracs_M.append(kmeansMajFracs)
+
+# output file:
+print("writing kmeans_M results to file")
+with open("results_kmeans_M.txt", 'w') as outf:
+	outf.write('\t'.join(['M', 'kmeans_obj', 'kmeans_majFrac_avg', 'kmeans_time']) + '\n')
+	for i in range(len(kmeans_M)):
+		outf.write("\t".join(
+			[str(var) for var in [ kmeans_M[i], kmeansObj_M[i], kmeansMajFrac_avg_M[i], kmeansTime_M[i] ]]
+		) + "\n")
+
+with open("results_kmeans_M_majorities.txt", 'w') as outf:
+	for i in range(len(kmeans_M)):
+		outf.write("\n".join(str(var) for var in kmeansMajPops_M[i] + kmeansMajFracs_M[i]) + "\n")
+	outf.write("\n")
+
 
 '''
 # repeat tests for EM
@@ -123,3 +186,8 @@ for N in range(10, 150, 10):
 	# measure EM with EM obj
 
 '''
+
+
+
+
+
