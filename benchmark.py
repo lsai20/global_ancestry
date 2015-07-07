eb4ee2d8a43959deaaf149bdc739ab86e3ab0811
LG <lg@s-164-67-229-99.resnet.ucla.edu>
2015-07-07 11:21:46 -0700

    now run pca once for quality trial, still nan problem

--- benchmark.py
+++ benchmark.py
@@ -38,6 +38,8 @@ def majorityPop(indivs, k):
 		majPops.append(mostCommonPop)
 		majFracs.append(frac)
 
+	# Note majPops[s] = majority pop of cluster s, majFracs[s] = % cluster s that is max 
+	# cluster sizes could be used to find weighted avg
 	return majPops, majFracs, clusterSizes
 
 
@@ -63,64 +65,33 @@ def runBenchmark(N=200, M=10000, K=3, usePCA=False, n_components=2):
 		return PCA_nocluster.pca_transform(indivs_copy, genoArr_copy, n_components)
 
 	if usePCA:
-		print("timing pca...") # avg 2 runs. genoArr_copy isn't changed from run-to-run
+		#print("timing pca...") # avg 2 runs. genoArr_copy isn't changed from run-to-run
 		#(indivs_copy does get changed, but shouldn't affect run since it restarts each time)
 		# unlike kmeans, pca is deterministic so runtime shouldn't vary. also pca step is slower, bottleneck.
 		genoArr_copy_geno = genoArr_copy  # make a copy of genotype data first (as opposed to components)
-
 		pcaTime = timeit.timeit(pca_i, number = 2)/2.0 
-		# also update genoArr_copy for kmeans 
+
+		# get components for genoArr_copy for kmeans (both timing and quality runs)
+		# pca deterministic, so don't need to rerun for each quality trial
 		pcaObj, genoArr_copy = PCA_nocluster.pca_transform(indivs_copy, genoArr_copy, n_components)
 
 
 	def kmeans_i(): # zero input fxn for timeit
 		return kmeans.kmeans(indivs_copy, genoArr_copy, K)
 
-	# timing kmeans, # run 10x per data pt, avg
-	print("timing k-means 10x...")
+	# timing kmeans, run 10x per data pt, avg
+	#print("timing k-means 10x...")
 	kmeansTime = timeit.timeit(kmeans_i, number = 10)/10.0 
 
+
 	### QUALITY ###
-	if usePCA: # reset indivs_copy before running pca
-		for i in range(len(indivs_copy)):
-			indivs_copy[i].geno = genoArr_copy_geno[i]
-
-
-	# TODO pca deterministic so only need to run once for all 10 quality trials
 	majFracAvgsByRun = np.zeros(10)
 
-	print("----")
-	print("genoArr_copy",genoArr_copy[0])
-	print("indivs_copy[0].geno", indivs_copy[0].geno)
-
-	if usePCA:
-		# make fresh copy of genoArr to run PCA on
-		genoArr_copy = genoArr_copy_geno
-		print("genoArr_copy_geno", genoArr_copy_geno[0])
-		pcaObj, genoArr_copy = PCA_nocluster.pca_transform(indivs_copy, genoArr_copy, n_components)}
-
-
 	for run in range(10):
-
-
-		print("genoArr_copy",genoArr_copy[0], len(genoArr_copy[0]))
-		print("indivs_copy[0].geno", indivs_copy[0].geno, len(indivs_copy[0].geno))
 		centers = kmeans.kmeans(indivs_copy, genoArr_copy, K, maxIter = 1000, verbose = False)
 		kmeansObj = kmeans.kmeansObj(indivs_copy, centers)
 		majPops, majFracs, clusterSizes = majorityPop(indivs_copy, K)
-
-		# majPops[s] = majority pop of cluster s
-		# majFracs[s] = % cluster s that is max 
-
-		# TODO should weight mean by size of cluster?
-		# TODO is it ok to weight homogeneity equal for cluster with 1 indiv and cluster with 1000
-		majFracAvgsByRun[run] = 1.0*sum(majFracs)/K
-
-		if usePCA: # reset from components to genotypes for next run
-			genoArr_copy = genoArr_copy_geno
-			for i in range(len(indivs_copy)):
-				indivs_copy[i].geno = genoArr_copy_geno[i]
-
+		majFracAvgsByRun[run] = 1.0*sum(majFracs)/K  # unweighted avg across clusters
 
 	majFrac_avg = np.mean(majFracAvgsByRun)
 	majFrac_std = np.std(majFracAvgsByRun)
@@ -138,7 +109,7 @@ def runBenchmark(N=200, M=10000, K=3, usePCA=False, n_components=2):
 # true K = 3 (CHB, MKK, CEU)
 
 indivs_, genoArr_ = parseHapmap.runParse()
-print("Using DEU, CHB and MKK") # TODO update if i change pops
+print("Using CEU, CHB and MKK") # TODO update if i change pops
 # though uneven pop size may be good to show weakness of kmeans
 
 def runBenchmark_multiNM(Nd, Md, K, usePCA = False, n_components = 10):
@@ -172,7 +143,6 @@ def runBenchmark_multiNM(Nd, Md, K, usePCA = False, n_components = 10):
 		pcaTime_N.append(pcaTime)
 
 
-
 	outfBase = 'results_pca%d_kmeans_N_M%d' % (n_components, Md)
 	if not usePCA:
 		outfBase = 'results_kmeans_N_M%d' % (Md)
@@ -185,7 +155,6 @@ def runBenchmark_multiNM(Nd, Md, K, usePCA = False, n_components = 10):
 			outf.write("\t".join(
 				[str(var) for var in [ kmeans_N[i], kmeansObj_N[i], majFrac_avg_N[i], majFrac_avg_N[i], kmeansTime_N[i], pcaTime_N[i] ]]
 			) + "\n")
-
 
 	# repeat for varying M, use first M snps
 	#print("Benchmarking K-means, varying M with N = %d, K = %d" % (N, K))
@@ -246,22 +215,21 @@ print("finished pca_transform")
 '''
 
 # testing with one benchmark
-runBenchmark(N=200, M=100, K=3, usePCA=True, n_components=2)
+#runBenchmark(N=200, M=100, K=3, usePCA=False, n_components=2)
 
 # run benchmarks with PCA for varying number of components
 
 # default values of N,M,K (when not varied for testing)
-N = 300
+N = 250
 M = 10000
 K = 3
 
-'''
 for n_components in [2, 10]: #[2,10,50,100]:
 	print("running n_components = %d" % n_components)
 	runBenchmark_multiNM(N,M,K,usePCA=True,n_components = n_components)
 
 runBenchmark_multiNM(N,M,K,usePCA=False, n_components = 0)
-'''
+
 
 
 
